15e96bdb6e8f4a640e742130e1f55ee0eb86d807
Danila Hanchar <d_hanchar@wargaming.net>
2017-09-28 16:39:23 +0300

    default 0 bug

--- flask_request_validator/validator.py
+++ flask_request_validator/validator.py
@@ -126,7 +126,7 @@ def __get_errors(params):
 
                 continue
             else:
-                if param.default:
+                if param.default is not None:
                     if isinstance(param.default, types.LambdaType):
                         value = param.default()
                     else:
